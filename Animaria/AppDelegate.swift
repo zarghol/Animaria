6a5653bfc1a884494c3e9d672df2ec22c6c24311
Clément NONN <clement.nonn@gmail.com>
2018-05-25 20:51:39 +0200

    Initial Commit

---
+++ Animaria/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Animaria
+//
+//  Created by Clément NONN on 25/05/2018.
+//  Copyright © 2018 Clément NONN. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
